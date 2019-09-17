524c31441f7943e42cc306ca09a55a866df5e734
Kazbek <kazbek.se@gmail.com>
2019-09-18 00:36:55 +0300

    Init.

---
+++ Basics/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Thread th1 = new Thread(Counter);
+            Thread th2 = new Thread(Counter);
+            Thread th3 = new Thread(Counter);
+            th1.Start();
+            th2.Start();
+            th3.Start();
+            Console.ReadKey();
+        }
+
+        public static void Counter() => Counter(10);
+
+        public static void Counter(int to)
+        {
+            for (int i = 0; i < to; i++)
+            {
+                Console.WriteLine($"Поток {Thread.CurrentThread.ManagedThreadId} досчитал до: {i}");
+                Thread.Sleep(new Random().Next(100, 10000));
+            }
+        }
+    }
+}
